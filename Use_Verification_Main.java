b02b86b96abe563325ba71ac0a96c5745f854182
VCHINIMI <vinaychinimilli@gmail.com>
2020-09-29 19:24:32 +0530

    [Vinay]Added: First Name Test Cases

--- Use_Verification_Main.java
+++ Use_Verification_Main.java
@@ -1,5 +1,18 @@
 package myPackageVinay;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Use_Verification_Main {
+
+	public boolean validatefName(String string) {
+		String regex ="^[A-Z]{1}[a-z]{2,}$";		 
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(string);		
+		if(matcher.matches()) 
+			return true;
+		else 
+			return false;		
+	}
 
 }
--- Use_Verification_MainTest.java
+++ Use_Verification_MainTest.java
@@ -1,14 +1,30 @@
 package myPackageVinay;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class Use_Verification_MainTest {
 
+class Use_Verification_MainTest {	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		Use_Verification_Main ucMain = new Use_Verification_Main();
+		boolean result = ucMain.validatefName("Vinay");
+		assertTrue(result);		
 	}
-
+	
+	@Test
+	public void givenFirstName_WhenNotEnoughCharacters_ShouldReturnFalse() {
+		Use_Verification_Main ucMain = new Use_Verification_Main();
+		boolean result = ucMain.validatefName("Vin");
+		assertFalse(result);		
+	}
+	
+	@Test
+	public void givenFirstName_WhenFirstCharNotCapital_ShouldReturnFalse() {
+		Use_Verification_Main ucMain = new Use_Verification_Main();
+		boolean result = ucMain.validatefName("vinay");
+		assertFalse(result);		
+	}	
+	
 }
